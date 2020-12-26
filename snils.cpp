447530cf11ea972d4c6e17c6e292f3caa53bb8fa
vlasiuk44 <55242330+vlasiuk44@users.noreply.github.com>
2020-12-26 23:25:22 +0800

    Create snils.cpp

---
+++ snils.cpp
@@ -0,0 +1,25 @@
+void z4() {
+    ofstream fit("new.txt");
+    setlocale(LC_ALL, "Russian");
+    for (int i = 100000000; i < 1000000000; i++)
+    {
+        if (prime(i)) {
+            int m = 9;
+            int summ = 0;
+            string k = to_string(i);
+            for (int j = 0; j < k.length(); j++)
+            {
+                summ += (k[j] - '0') * m;
+                m--;
+            }
+            if (summ % 101 == summ) {
+                fit << i << "\n";
+            }
+        }
+    }
+}
+
+int main() {
+    z4();
+   
+}
