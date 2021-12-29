0c8e8ec43ec907fb74f4c2f70b7bc805ede56c64
vlasiuk44 <55242330+vlasiuk44@users.noreply.github.com>
2021-12-29 21:57:26 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,70 +1,114 @@
-import csv
-import os
-otbor = ['GH', 'CE', 'PL', 'CBM', 'GT', 'AA']
-niche = {}  # словарь отобраных бактерий с нишами
-items = []  # массив всех файлов, с которыми взаимодействуем
-vectors = []  # все векторы
-vectors2_new = []
-vectors2 = []
-vectors_dict = []  # массив всех 560 бактерий, ключ - бактерия, значение - количество
-enzymes = {}  # ключ - фермент, значения - все бактерии, в которых он есть
-
-with open('niche2.csv') as File2:
-    reader = csv.reader(File2, delimiter=';', quotechar=',',
-                        quoting=csv.QUOTE_MINIMAL)
-    for row in reader:
-        niche[row[0]] = row[1]  # {'GCF_000064305.2': 'aquatic', 'GCF_000240075.2': 'aquatic', 'GCF_000252125.1': 'aquatic'...}
-
-folder = 'C:/PyProj/pythonProject4/inp'
-for subdir, dirs, files in os.walk(folder):
-    for item in os.listdir(folder):  # перебор всех файлов
-        if 'sum' in item:  # отбираем нужные файлы
-
-            bac = 'inp/' + item  # строка - путь файла
-            with open(bac) as File:  # открыть файл
-                if os.stat(bac).st_size == 0: continue  # файл пустой
-                items.append(item)  # добавляем в массив названия файлов
-                dict = {}  # ключ - фермент, значение - его количество в бактерии
-                reader = csv.reader(File, delimiter=',', quotechar=',',
-                                    quoting=csv.QUOTE_MINIMAL)
-                for row in reader:
-
-                    for k in [x.strip() for y in row[0].split()[5].split('-') for x in
-                              y.split(';')]:  # строка с ферментами, разбитая в массив
-                        if k == 'pattern': continue
-
-                        if '_' in k: k = str(k[:k.find('_')])  # игнорируем все _
-
-                        if k not in enzymes.keys():
-                            enzymes[k] = [item]  # добавляем ферменты
-                        else:
-                            if item not in enzymes[k]:
-                                enzymes[k].append(item)  # добавляем бактерии
-                        if k not in dict:
-                            dict[k] = 1  # создаем фермент
-                        else:
-                            dict[k] += 1  # считаем кол-во ферментов
-                        dict_new = {'GH':0, 'CE':0, 'PL':0, 'CBM':0, 'GT':0, 'AA':0}  # словарь для подсчета ненулевых ферментов
-                        for h in dict.keys():
-                            if h == 'susC' or h == 'susD' or h == 'unk': continue
-                            for char in h:
-                                if char.isdigit():
-                                    h = h[:h.index(char)]
-                                    # print(h)
-                                    break
-                            if h not in dict_new:
-                                dict_new[h] = 1
-                            else:
-                                dict_new[h] += 1
-
-            vectors_dict.append(dict)
-            vectors.append(list(dict_new.values()))
-
-for i in range(len(vectors)):
-    if items[i][:15] in list(niche.keys()) and niche[items[i][:15]]=='freshwater':
-
-        print(vectors[i],end=',\n')
-
-print(list(dict_new.keys()))
+import turtle
+from tkinter import *
+from tkinter import ttk
 
 
+def create(iters, axiom, rules):
+    start_string = axiom
+    if iters == 0:
+        return axiom
+    end_string = ""
+    for _ in range(iters):
+        end_string = "".join(rules[i] if i in rules else i for i in start_string)
+        start_string = end_string
+
+    return end_string
+
+
+def draw_sys(t, instructions, angle, distance):
+    for cmd in instructions:
+        if cmd == 'F':
+            t.forward(distance)
+        elif cmd == '+':
+            t.right(angle)
+        elif cmd == '-':
+            t.left(angle)
+
+
+def launch(length=4, size=2, y_offset=0,
+           x_offset=0, offset_angle=0, width=1000, height=1000):
+    figures = {
+        "Koch's Snowflake": {
+            "axiom": "F--F--F",
+            "rules": {"F": "F+F--F+F"},
+            "iterations": 4,
+            "angle": 60
+        },
+        "Crystal": {
+            "axiom": "F+F+F+F",
+            "rules": {"F": "FF+F++F+F"},
+            "iterations": 3,
+            "angle": 90
+        },
+        "Vicek's Fractal": {
+            "axiom": "F-F-F-F",
+            "rules": {"F": "F-F+F+F-F"},
+            "iterations": 4,
+            "angle": 90,
+        },
+        "Sierpinski Carpet": {
+            "axiom": "YF",
+            "rules": {"X": "YF+XF+Y", "Y": "XF-YF-X"},
+            "iterations": 6,
+            "angle": 60
+        },
+    }
+
+    colors = {
+        "Black": "black",
+        "Red": "red",
+        "Green": "green",
+        "Blue": "blue"
+    }
+
+    iterations = figures[figure.get()]["iterations"]
+    axiom = figures[figure.get()]["axiom"]
+    rules = figures[figure.get()]["rules"]
+    angle = figures[figure.get()]["angle"]
+
+    inst = create(iterations, axiom, rules)
+
+    wn = turtle.Screen()
+    wn.clear()
+
+    t = turtle.Turtle()
+    t.hideturtle()
+    wn.setup(width, height)
+    t.pencolor(colors[color_box.get()])
+    t.up()
+    t.backward(-x_offset)
+    t.left(90)
+    t.backward(-y_offset)
+    t.left(offset_angle)
+    t.down()
+    t.speed(0)
+    t.pensize(size)
+
+    draw_sys(t, inst, angle, length)
+
+
+window = Tk()
+window.geometry("300x100")
+
+figure_lbl = Label(window, text="Fractal: ")
+figure_lbl.grid(column=0, row=0)
+figure = ttk.Combobox(window)
+figure["values"] = ("Koch's Snowflake", "Crystal", "Vicek's Fractal", "Sierpinski Carpet")
+figure.current(0)
+figure.grid(column=1, row=0)
+
+color_lbl = Label(window, text="Color: ")
+color_lbl.grid(column=0, row=1)
+color_box = ttk.Combobox(window)
+color_box["values"] = ("Black", "Red", "Green", "Blue")
+color_box.current(0)
+color_box.grid(column=1, row=1)
+
+space_lbl = Label(window, text="")
+space_lbl.grid(column=0, row=3)
+
+btn = Button(window, text="Launch", command=launch)
+btn.grid(column=1, row=4)
+
+window.protocol("WM_DELETE_WINDOW", window.destroy)
+window.mainloop()
